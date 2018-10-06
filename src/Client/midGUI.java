238bb2f39d86519db2a540ae43e8fc81583d698f
max995 <max995@126.com>
2018-10-07 00:09:46 +1000

    sadad

--- src/Client/midGUI.java
+++ src/Client/midGUI.java
@@ -1,12 +1,18 @@
 package Client;
 
+import java.awt.BorderLayout;
+import java.awt.Container;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class midGUI {
 
 	private JFrame frame;
+	private Container Cplayer;
+	private Container Ctable;
+
 
 	public midGUI() {
 		initialize();
@@ -17,6 +23,24 @@ public class midGUI {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 1000, 1000);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new BorderLayout(10,10));
+		showPlayer();
+		showTable();
+
 	}
+
+	private void showTable() {
+		frame.getContentPane().add(Ctable,BorderLayout.CENTER);
+		
+	}
+
+	private void showPlayer() {
+		frame.getContentPane().add(Cplayer,BorderLayout.WEST);
+		JLabel playerLabel = new JLabel("Players:");
+		Cplayer.add(playerLabel,BorderLayout.NORTH);
+
+		
+	}
+	
 
 }
